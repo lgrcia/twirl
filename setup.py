55c893cd080a60f01b00fd4aa879f3d21d99106b
Lionel <lionel_garcia@live.fr>
2021-04-27 11:20:41 +0200

    hange version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="twirl",
-    version="0.0.1",
+    version="0.0.2",
     author="Lionel J. Garcia",
     description="python-only astrometric plate solving",
     long_description=README,
