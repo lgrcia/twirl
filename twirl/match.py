218fd180255b8fca60c5b72efe413c3c4fe51519
lgrcia <lionel_garcia@live.fr>
2023-05-15 16:02:57 +0200

    refactor: refactor and document

--- twirl/match.py
+++ twirl/match.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import numpy as np
 
 from twirl.geometry import get_transform_matrix, pad
@@ -5,17 +7,53 @@ from twirl.quads import hashes as hash4
 from twirl.triangles import hashes as hash3
 
 
-def count_cross_match(xy1, xy2, tol=1e-3):
+def count_cross_match(coords1, coords2, tol=1e-3):
+    """
+    Counts the number of cross-matches between two sets of 2D points.
+
+    Parameters
+    ----------
+    coords1 : np.ndarray
+        First set of points coordinates, shape (n, 2).
+    coords2 : np.ndarray
+        Second set of points coordinates, shape (m, 2).
+    tol : float, optional
+        Tolerance of the match, by default 1e-3.
+
+    Returns
+    -------
+    int
+        Number of cross-matches between coords1 and coords2.
+    """
     return np.count_nonzero(
-        np.min(np.linalg.norm(xy1[:, None, :] - xy2[None, :, :], axis=-1), 0) < tol
+        np.min(np.linalg.norm(coords1[:, None, :] - coords2[None, :, :], axis=-1), 0)
+        < tol
     )
 
 
-def cross_match(xy1, xy2, tolerance=10):
+def cross_match(coords1, coords2, tolerance=10):
+    """
+    Finds the closest matches between two sets of 2D points.
+
+    Parameters
+    ----------
+    coords1 : np.ndarray
+        First set of points coordinates, shape (n, 2).
+    coords2 : np.ndarray
+        Second set of points coordinates, shape (m, 2).
+    tolerance : float, optional
+        Tolerance of the match, given in coords1 points units, by default 10.
+
+    Returns
+    -------
+    np.ndarray
+        Array of matched indices, where each row contains the indices of the matched points in coords1 and coords2.
+    """
+
     matches = []
 
-    for i, s in enumerate(xy1):
-        distances = np.linalg.norm(s - xy2, axis=1)
+    for i, s in enumerate(coords1):
+        distances = np.linalg.norm(s - coords2, axis=1)
         closest = np.argmin(distances)
         if distances[closest] < tolerance:
             matches.append([i, closest])
@@ -24,55 +62,59 @@ def cross_match(xy1, xy2, tolerance=10):
 
 
 def find_transform(
-    xy2: np.ndarray,
-    xy1: np.ndarray,
+    coords2: np.ndarray,
+    coords1: np.ndarray,
     tolerance: int = 12,
-    min_match: int = None,
+    min_match: Optional[int] = None,
     asterism: int = 4,
 ) -> np.ndarray:
-    """_summary_
+    """
+    Finds the transformation matrix that maps the coordinates in `coords2` to the coordinates in `coords1`.
 
     Parameters
     ----------
-    xy2 : np.ndarray
-        first set of points coordinates, shape (n, 2)
-    xy1 : np.ndarray
-        second set of points coordinates, shape (m, 2)
+    coords2 : np.ndarray
+        The coordinates to be transformed, shape (n, 2).
+    coords1 : np.ndarray
+        The target coordinates, shape (m, 2).
     tolerance : int, optional
-        tolerance of the match, given in xy1 points units, by default 12
-    min_match : _type_, optional
-        if None, stops when the number of crossed matched points
-        is min_match, by default None
+        The tolerance of the match, given in `coords1` points units, by default 12.
+    min_match : Optional[int], optional
+        The minimum number of matches required to stop the search, by default None.
+    asterism : int, optional
+        The asterism to use for hashing, either 3 or 4, by default 4.
 
     Returns
     -------
-    _type_
-        _description_
+    np.ndarray
+        The transformation matrix that maps `coords2` to `coords1`.
     """
 
     if asterism == 3:
-        asterism = hash3
+        asterism_function = hash3
     elif asterism == 4:
-        asterism = hash4
+        asterism_function = hash4
     else:
         raise ValueError("available asterisms are 3 and 4")
 
-    hash1, points1 = asterism(xy1)
-    hash2, points2 = asterism(xy2)
+    hash1, asterism_coords1 = asterism_function(coords1)
+    hash2, asterism_coords2 = asterism_function(coords2)
     distances = np.linalg.norm(hash1[:, None, :] - hash2[None, :, :], axis=2)
     shortest_hash = np.argmin(distances, 1)
     ns = []
 
     for i, j in enumerate(shortest_hash):
-        M = get_transform_matrix(points2[j], points1[i])
-        test = (M @ pad(xy2).T)[0:2].T
-        n = count_cross_match(xy1, test, tolerance)
+        M = get_transform_matrix(asterism_coords2[j], asterism_coords1[i])
+        test = (M @ pad(coords2).T)[0:2].T
+        n = count_cross_match(coords1, test, tolerance)
         ns.append(n)
         if min_match is not None:
             if n >= min_match:
                 break
 
         i = np.argmax(ns)
-        M = get_transform_matrix(points2[np.argmin(distances, 1)[i]], points1[i])
+        M = get_transform_matrix(
+            asterism_coords2[np.argmin(distances, 1)[i]], asterism_coords1[i]
+        )
 
     return M
--- twirl/triangles.py
+++ twirl/triangles.py
@@ -6,6 +6,22 @@ from twirl.geometry import triangle_angles
 
 
 def order_points(triangles):
+    """
+    Orders the vertices of each triangle in a consistent manner.
+
+    Vertices are ordered by their distance to the centroid of the triangle.
+
+    Parameters
+    ----------
+    triangles : ndarray
+        An array of shape (n_triangles, 3, 2) representing the vertices of each triangle.
+
+    Returns
+    -------
+    ndarray
+        An array of shape (n_triangles, 3, 2) with the vertices of each triangle ordered consistently.
+    """
+
     # Compute the centroid of the triangles
     centroids = np.mean(triangles, axis=1, keepdims=True)
 
@@ -22,6 +38,24 @@ def order_points(triangles):
 
 
 def hashes(xy, min_angle=np.deg2rad(30)):
+    """
+    Computes the hashes of the triangles formed by the points in xy.
+
+    Parameters
+    ----------
+    xy : ndarray
+        An array of shape (n_points, 2) representing the x and y coordinates of each point.
+    min_angle : float, optional
+        The minimum angle (in radians) that a triangle must have to be included in the hashes. Default is 30 degrees.
+
+    Returns
+    -------
+    hashes : ndarray
+        An array of shape (n_hashes, 2) representing the hashes of the triangles.
+    triangles : ndarray
+        An array of shape (n_triangles, 3, 2) representing the vertices of each triangle.
+    """
+
     triangles_idxs = np.array(list(combinations(np.arange(xy.shape[0]), 3)))
     triangles = xy[triangles_idxs]
     triangles = order_points(triangles)
